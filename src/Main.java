cd8b1ba464406988c42fad6086da6ab68fb3b98c
Mariusz Rychlicki <mariuszrychlicki04@gmail.com>
2024-03-21 17:47:45 +0100

    Added and implemented RR comparator, modified polling algorithm.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ import java.util.PriorityQueue;
 import java.util.Random;
 
 public class Main {
-    static Comparator<Process> QueueComparator = new Process.SJFwComparator();
+    static Comparator<Process> QueueComparator = new Process.FCFSComparator();
     static PriorityQueue<Process> processQueue = new PriorityQueue<>(QueueComparator);
     static ArrayList<Process> completedProcesses = new ArrayList<>();
     static int iterations = 1_000_000;
-    static int initialProcesses = 50;
+    static int initialProcesses = 0;
     static Random r = new Random();
+    static int switchCount = -1;
 
     public static void generateProcesses(int t) {
         double gaussianSample = r.nextGaussian() * (iterations / 6.0) + (iterations / 2.0);
@@ -22,29 +23,41 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        for(int i = 0; i < initialProcesses; i++) {
+        for(int t = 0; t < initialProcesses; t++) {
             Process randomProcess = Process.generateRandomProcess(0);
 
             processQueue.add(randomProcess);
         }
 
+        Process current = null;
         for(int t = 0; t < iterations; t++) {
             generateProcesses(t);
 
-            Process current = processQueue.peek();
-
-            if(current == null) {
+            Process next = processQueue.poll();
+            if(next == null) {
                 continue;
             }
 
-            if(current.advanceProcess(1, t)) {
-                processQueue.remove(current);
+            switchCount += (next.equals(current)) ? 0 : 1;
+            current = next;
+
+            boolean completed = current.advanceProcess(1, t);
+
+            if(completed) {
                 completedProcesses.add(current);
+            } else {
+                processQueue.add(current);
             }
         }
 
         int aggregateSumProcessLength = completedProcesses.stream()
-                .map(Process::getProcessLength)
+                .map(Process::getCompleted)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        int aggregateSumBegunProcessLength = aggregateSumProcessLength + processQueue.stream()
+                .map(Process::getCompleted)
+                .filter(completed -> completed > 0)
                 .mapToInt(Integer::intValue)
                 .sum();
 
@@ -60,6 +73,12 @@ public class Main {
                 .average()
                 .orElse(0.0);
 
-        System.out.printf("Average completion time for a process: %f%nAverage process wait time since initialization: %f%nTotal sum of all process lengths: %d%nTotal processes served: %d", avgCompletion, avgWaitTime, aggregateSumProcessLength, completedProcesses.size());
+        System.out.printf("Average completion time for a process: %f%n" +
+                "Average process wait time since initialization: %f%n" +
+                "Sum of completed process lengths: %d%n" +
+                "Sum of begun process lengths (including completed): %d%n" +
+                "Total processes served: %d%n" +
+                "Total switch count: %d",
+                avgCompletion, avgWaitTime, aggregateSumProcessLength, aggregateSumBegunProcessLength, completedProcesses.size(), switchCount);
     }
 }
--- src/Process.java
+++ src/Process.java
@@ -3,30 +3,33 @@ import java.util.Random;
 import java.util.UUID;
 
 public class Process {
-    private final UUID PID;
+    private static int processNumber = 1;
+    private final int PID;
     private final int processLength;
     private final int processInitializationTime;
     private int startTime;
     private int completionTime;
     private String processName;
     private int completed;
+    private int processorAccessCount;
 
     public Process(String processName, int processLength, int processInitializationTime) {
-        this.PID = UUID.randomUUID();
+        this.PID = processNumber++;
         this.processName = processName;
         this.processLength = processLength;
         this.processInitializationTime = processInitializationTime;
         this.completed = 0;
         this.startTime = -1;
         this.completionTime = -1;
+        this.processorAccessCount = 0;
     }
 
     public static Process generateRandomProcess(int processInitializationTime) {
         Random r = new Random();
-        return new Process("RandomProcess" + r.nextInt(), 1 + r.nextInt(9999), processInitializationTime);
+        return new Process("RandomProcess" + r.nextInt(), 1 + r.nextInt(999), processInitializationTime);
     }
 
-    public UUID getPID() {
+    public int getPID() {
         return PID;
     }
 
@@ -62,9 +65,18 @@ public class Process {
         this.processName = processName;
     }
 
+    public int getProcessorAccessCount() {
+        return processorAccessCount;
+    }
+
+    public void setProcessorAccessCount(int processorAccessCount) {
+        this.processorAccessCount = processorAccessCount;
+    }
+
     public boolean advanceProcess(int units, int time) {
         this.startTime = (this.startTime < 0) ? time : this.startTime;
         this.completed += units;
+        this.processorAccessCount++;
 
         if(this.completed >= this.processLength) {
             this.completionTime = time;
@@ -75,15 +87,24 @@ public class Process {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if(!(o instanceof Process p)) {
+            return false;
+        }
+
+        return p.getPID() == this.PID;
+    }
+
+    @Override
     public String toString() {
-        return String.format("PID:%s, initialized: %d, started: %d, completed: %d/%d", this.PID, this.processInitializationTime, this.startTime, this.completed, this.processLength);
+        return String.format("initialized: %d, started: %d, completed: %d/%d, accessed: %d", this.processInitializationTime, this.startTime, this.completed, this.processLength, this.processorAccessCount);
     }
 
     public static class FCFSComparator implements Comparator<Process> {
         // Comparator which seeks to determine which process was initialized first, aka First Come, First Served.
         @Override
         public int compare(Process o1, Process o2) {
-            return Integer.compare(o1.processInitializationTime, o2.processInitializationTime);
+            return Integer.compare(o1.getPID(), o2.getPID());
         }
     }
 
@@ -100,6 +121,21 @@ public class Process {
         @Override
         public int compare(Process o1, Process o2) {
             return Integer.compare(o1.getProcessLength(), o2.getProcessLength());
+        }
+    }
+
+    public static class RRComparator implements Comparator<Process> {
+        private final Comparator<Process> baseComparator = new FCFSComparator();
+        private final int q;
+
+        public RRComparator(int q) {
+            this.q = q;
+        }
+
+        @Override
+        public int compare(Process o1, Process o2) {
+            int res = Integer.compare(o1.getProcessorAccessCount() / this.q, o2.getProcessorAccessCount() / this.q);
+            return (res != 0) ? res : baseComparator.compare(o1, o2);
         }
     }
 }
